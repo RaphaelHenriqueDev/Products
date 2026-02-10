2c213c8794db421dd9644fe35f20440385e67bf5
RaphaelHenriqueDev <raphaelhenriquealvescosta@gmail.com>
2026-02-10 10:29:30 -0300

    feat: adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('API for managing products')
+    .setVersion('1.0')
+    .build();
+  const document = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
